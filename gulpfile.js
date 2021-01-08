68ee09bbfd77ea5c19445415526ecf0efe4de95a
AGrush <grushevskiy@gmail.com>
2021-01-08 11:44:34 +0000

    incremental builds

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,23 @@
 var gulp = require("gulp");
 var replace = require('gulp-string-replace');
 var minifyInline = require('gulp-minify-inline');
+var rename = require('gulp-rename');
 var htmlmin = require('gulp-htmlmin');
+
+
+//paths
+const paths = {
+  html: {
+    src: 'src/*.{html,htm}',
+    dest: 'dist/'
+  }
+}
 
 //gulp move
 function move() {
   return (
     gulp
-        .src("src/*.html")
+        .src(paths.html.src, {since: gulp.lastRun(move)})
 
 
         .pipe(replace('""', '"'))
@@ -20,9 +30,19 @@ function move() {
 
         //mini css js
         .pipe(minifyInline())
+
+        //rename
+        .pipe(rename({extname: '.DIST.html'}))
         
-        .pipe(gulp.dest("dist"))
+        .pipe(gulp.dest(paths.html.dest))
   );
 };
+
+//watcher //use done to signal async completion
+gulp.task('watch', function(done){
+  gulp.watch(paths.html.src, move); 
+  done();
+});
+
 
 exports.move = move;
